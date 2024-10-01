f15489de375e0e99efc1e3703056f8b109f7cb59
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2024-10-01 15:29:36 +0200

    static workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'akfgfragments.com',
-            port: '',
-            pathname: '/wp-content/uploads/**',
-          },
-        ],
-      }
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'akfgfragments.com',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+  }
 }
 
 export default nextConfig
